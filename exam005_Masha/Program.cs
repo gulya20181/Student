177d10b6f81cfa0a717fce7f3506ddff5d76a58f
Гуля <gulya20181@yandex.ru>
2022-09-04 15:33:43 +0300

    Masha

---
+++ exam005_Masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine ("Ура! Это же Masha");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
