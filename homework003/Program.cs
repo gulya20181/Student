b80063595ef22db6887bef906489bd5cb63f0df7
Гуля <gulya20181@yandex.ru>
2022-09-08 01:00:22 +0300

    homework003

---
+++ homework003/Program.cs
@@ -0,0 +1,9 @@
+﻿//напишите программу которая на вход принимает число N,
+// а на выходе показывает все четные числа от 1 до N. 
+Console.WriteLine("введите число");
+int i =1;
+int n  = Convert.ToInt32(Console.ReadLine());
+if ( i % 2 ==0)
+for ( i =2; i<=n ; i+=2);
+
+Console.WriteLine( "чётное");
