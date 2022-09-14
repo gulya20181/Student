7fad52e122810fc5c709cb4c3f01c58d6db7a129
Гуля <gulya20181@yandex.ru>
2022-09-14 12:57:09 +0300

    homework006

--- homework005/Program.cs
+++ homework005/Program.cs
@@ -3,7 +3,7 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 Console.WriteLine("введите число");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 int first =num, second =num;
 if (num < 100 & num > -100) 
 Console.WriteLine("третьего числа нет");
