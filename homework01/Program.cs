ff60a69c3aa787d978bdac60ed62fe80866d16d1
Гуля <gulya20181@yandex.ru>
2022-10-02 22:26:25 +0300

    homework01

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -1,146 +1,68 @@
-﻿//  № 54 Задайте двумерный массив. Напишите программу,
-// которая упорядочит по убыванию элементы каждой строки двумерного массива.
-int[,] numbers = new int[3, 4];
-FillArrayRandom(numbers);
-PrintArray(numbers);
-SortToLower(numbers);
-Console.WriteLine();
-PrintArray(numbers );
-void FillArrayRandom(int[,] array)
+﻿//№ 64 Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+int n = InputInt("Введите положительное число");
+int m = 1;
+if (n < 1)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
+    Console.WriteLine("Ввели не положительное число");
 }
-void SortToLower(int[,] array)// сортировка по убыванию
+Console.WriteLine(NaturalNumber(n, m));
+int NaturalNumber(int n, int m)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-     {
-        for (int k = 0; k < array.GetLength(1) - 1; k++)
- {
-         if (array[i, k] < array[i, k + 1])
-        {
-         int temp = array[i, k + 1];
-        array[i, k + 1] = array[i, k];
-         array[i, k] = temp;
-         }
- }
+    if (n == m)  return n;
+    else
+    Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
 }
- }
-}
-void PrintArray(int[,] array) // ф-я ввода 2х мерного массива
+int InputInt(string output)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-    for (int j = 0; j < array.GetLength(1); j++)
-     {
-     Console.Write($"{array[i, j]} ");
-    }
-        Console.WriteLine();
-    }
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
 }
 
 
-// № 56 Задайте прямоугольный двумерный массив. Напишите программу, 
-//которая будет находить строку с наименьшей суммой элементов.
-//Например, задан массив:1 4 7 2, 5 9 2 3,  8 4 2 4, 5 2 6 7
-Console.WriteLine("массив");
-     int[,] matrix =
-      {{1,4,7,2},
-      {5,9,2,3},
-      {8,4,2,4},
-      {5,2,6,7}};
-    int sum = 0;
-     int index = 0;
-   for (int i = 0; i < 3; i++)
- {
-     int temp = 0;
-    for (int j = 0; j < 4; j++)
- {
-  temp += matrix[j, i];
- }
-    if (temp > sum)
- {
-  sum = temp;
-  index = i;
- }
- }
-   Console.WriteLine("Столбец: ");
-   for (int i = 0; i < 4; i++)
-   {
-    Console.WriteLine(matrix[i, index]);
-   }  
 
-   
+// №66 Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120  M = 4; N = 8. -> 30
+int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
 
-   // №58 Задайте две матрицы. Напишите программу, которая будет
-// находить произведение двух матриц.
-
-Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
-int m = InputNumbers("Введите число строк 1-й матрицы ");
-int n = InputNumbers("Введите число столбцов 1-й матрицы ");
-int p = InputNumbers("Введите число столбцов 2-й матрицы ");
-int range = InputNumbers("Введите диапазон случайных чисел от 1 до ");
-
-int[,] firstMartrix = new int[m, n];
-CreateArray(firstMartrix);
-Console.WriteLine($"\n Первая матрица");
-WriteArray(firstMartrix);
-
-int[,] secomdMartrix = new int[n, p];
-CreateArray(secomdMartrix);
-Console.WriteLine($"\n Вторая матрица:");
-WriteArray(secomdMartrix);
-int[,] resultMatrix = new int[m,p];
-MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-Console.WriteLine($"\nПроизведение первой и второй матриц");
-WriteArray(resultMatrix);
-void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+int InputInt(string output)
 {
-  for (int i = 0; i < resultMatrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < resultMatrix.GetLength(1); j++)
-    {
-      int sum = 0;
-      for (int k = 0; k < firstMartrix.GetLength(1); k++)
-      {
-        sum += firstMartrix[i,k] * secomdMartrix[k,j];
-      }
-      resultMatrix[i,j] = sum;
-    }
-  }
-}
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i,j] + " ");
-    }
-    Console.WriteLine();
-  }
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
 }
 
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
+
+
+
+
+
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
